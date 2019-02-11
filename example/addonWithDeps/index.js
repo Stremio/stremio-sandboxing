5eedf04f94d22ebcb48ebc6f670e18e858bb2383
Ivo Georgiev <ivo@strem.io>
2019-02-12 00:26:26 +0200

    new example added

---
+++ example/addonWithDeps/index.js
@@ -0,0 +1,28 @@
+const fetch = sandbox_require('fetch')
+const animated = require('./animated')
+const isOdd = require('is-odd')
+
+function manifest() {
+	return Promise.resolve({
+		id: 'org.example.sandboxed',
+		name: 'Sandboxed addon',
+		version: '1.0',
+		description: 'Sandboxed much?',
+		catalogs: [{type: 'movie', id: 'animated'}],
+		resources: ['catalog'],
+	})
+}
+
+function get(resource, type, id) {
+	if (resource == 'catalog') {
+		const metas = animated.metas.map((m, i) => {
+			if (isOdd(i)) m.name = `odd: ${m.name}`
+			return m
+		})
+	} else {
+		Promise.reject('failed')
+	}
+	// @TODO should we test require-ing fs here, to show it's really sandboxed?
+}
+
+module.exports = { manifest, get }
