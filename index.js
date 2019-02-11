f97e69ad4d51e184cb64e4beb9e2186f0edf96ea
Ivo Georgiev <ivo@strem.io>
2019-02-11 22:44:48 +0200

    safeFetch

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const { Request } = require('node-fetch')
 const fetch = require('node-fetch')
+const url = require('url')
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+function safeFetch(input, init) {
+	// per spec, input can be a USVString or a Request
+	const request = new Request(input)
+	const parsed = url.parse(request.url)
+	console.log(parsed.hostname)
+	return fetch(request, init)
+}
 
 const exampleAddon = require('./example/proxyAddon')
 
-exampleAddon({ fetch }).manifest().then(m => console.log(m))
+
+exampleAddon({ fetch: safeFetch }).manifest().then(m => console.log(m))
