62b82daa312f371ea8214f39126482b494739c05
Ivo Georgiev <ivo@strem.io>
2019-02-11 23:09:17 +0200

    more vm stuff

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const vm = require('vm')
 const makeSafeFetch = require('./safeFetch')
 
+const runOptions = {
+	timeout: 1000,
+	breakOnSigint: true,
+}
 const deps = {
 	// @TODO storage
 	fetch: makeSafeFetch({
@@ -11,9 +15,10 @@ vm.createContext(deps)
 
 const exampleAddonCode = require('fs').readFileSync('./example/proxyAddon.js').toString()
 
+// @TODO check if this one can throw if invalid code is passed
 const addonScript = new vm.Script(exampleAddonCode)
 //const addonScriptData = addonScript.createCachedData()
 
-addonScript.runInContext(deps, { timeout: 500 })
+addonScript.runInContext(deps, runOptions)
 
 deps.manifest().then(manifest => console.log(manifest))
